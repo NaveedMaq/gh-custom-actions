8f6ca758d5fe3f3ad8f0f7c6417a445003415ce1
NaveedMaq <naveedmaqbool30@gmail.com>
2023-12-07 22:06:23 +0530

    add custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,16 @@
 const core = require("@actions/core");
+const exec = require("@actions/exec");
 
 function run() {
+  // 1) Get input values
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  // 2) Upload files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice("Hello from my custom javascript action");
 }
 
