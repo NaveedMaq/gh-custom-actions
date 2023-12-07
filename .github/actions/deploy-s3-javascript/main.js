4bb2411ff5d7cc0da970f24c99e3c0c2c934ce0d
NaveedMaq <naveedmaqbool30@gmail.com>
2023-12-07 22:14:36 +0530

    output utl

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -11,7 +11,9 @@ function run() {
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-  core.notice("Hello from my custom javascript action");
+  // 3) Output s3 url
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
 }
 
 run();
