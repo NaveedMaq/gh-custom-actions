355b006faaea5dc13998ccadfbde3c4b28b71451
NaveedMaq <naveedmaqbool30@gmail.com>
2023-12-08 09:37:45 +0530

    test s3 deployemnt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import MainContent from './components/MainContent';
-import logo from './assets/images/logo.png';
+import MainContent from "./components/MainContent";
+import logo from "./assets/images/logo.png";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>Learn & Master GitHub Actions</h1>
+        <h1>Learn and Master GitHub Actions</h1>
       </header>
       <MainContent />
     </>
